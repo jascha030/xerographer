ce597ad9f4d2051902964905cb551a20c3c46981
Jascha030 <contact@jaschavanaalst.nl>
2021-10-02 23:49:38 +0200

    Started binary for console commands

---
+++ includes/loader.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+$autoloader = static function () {
+    $locations = [
+        dirname(__FILE__, 2) . '/vendor/autoload.php',
+        dirname(__FILE__, 4) .  '/autoload.php',
+        getenv('HOME') . '/.composer/vendor/autoload.php',
+    ];
+
+    foreach ($locations as $autoloaderPath) {
+        if (is_file($autoloaderPath)) {
+            return $autoloaderPath;
+        }
+    }
+
+    $errorMsg = sprintf(
+        'Couldn\'t find Composer\'s Autoloader file in any of the following paths: 
+                %s, please make sure you run the %s or %s commands.',
+        implode(', ', $locations),
+        '<pre>composer install --prefer-source</pre>',
+        '<pre>composer dump-autoload</pre>'
+    );
+
+    throw new \RuntimeException($errorMsg);
+};
+
+/** @noinspection PhpIncludeInspection */
+require_once $autoloader();
