4e1317c6d9900140ed753925cc586fb5fe445fed
Jascha030 <contact@jaschavanaalst.nl>
2021-10-03 01:47:34 +0200

    Container definitions

--- config/container-config.php
+++ config/container-config.php
@@ -1,16 +1,39 @@
 <?php
 
+use Jascha030\Xerox\Database\DatabaseServiceInterface;
 use Jascha030\Xerox\Twig\TwigTemplater;
 use Psr\Container\ContainerInterface;
+use Symfony\Component\Console\Question\Question;
 use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 use Twig\Loader\LoaderInterface;
 
 return [
-    LoaderInterface::class => DI\create()
-    Environment::class => function (LoaderInterface $loader) {
-        return new Environment($loader);
+    'twig.root' => dirname(__DIR__) . '/templates',
+    /**
+     * Twig classes
+     */
+    LoaderInterface::class => static function (ContainerInterface $c) {
+        return new FilesystemLoader($c->get('twig.root'));
     },
-    TwigTemplater::class => function (ContainerInterface $c) {
+    Environment::class => static function (ContainerInterface $c) {
+        return new Environment($c->get(LoaderInterface::class));
+    },
+    TwigTemplater::class => static function (ContainerInterface $c) {
         return new TwigTemplater($c->get(Environment::class));
-    }
+    },
+    /**
+     * Questions
+     */
+    'command.init.questions.user' => static function (ContainerInterface $c) {
+        return new Question('Enter mysql username');
+    },
+    'command.init.questions.password' => static function (ContainerInterface $c) {
+        $question = new Question('Password');
+        $question->setHidden(true);
+        $question->setHiddenFallback(false);
+
+        return $question;
+    },
+
 ];
