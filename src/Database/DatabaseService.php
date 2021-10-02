31a447128c5105ca53da97621d48bd4b4472bda7
Jascha030 <contact@jaschavanaalst.nl>
2021-10-03 00:32:58 +0200

    Database service

---
+++ src/Database/DatabaseService.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Jascha030\Xerox\Database;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DriverManager;
+
+class DatabaseService implements DatabaseServiceInterface
+{
+    private string $user;
+
+    private string $password;
+
+    private string $host;
+
+    private string $driver;
+
+    private Connection $connection;
+
+    public function __construct(
+        string $user,
+        string $password,
+        string $host = 'localhost',
+        string $driver = 'pdo_mysql'
+    ) {
+        $this->user = $user;
+        $this->password = $password;
+        $this->host = $host;
+        $this->driver = $driver;
+    }
+
+    /**
+     * @throws \Doctrine\DBAL\Exception
+     */
+    private function connect(): DatabaseServiceInterface
+    {
+        $params = [
+            'user'     => $this->user,
+            'password' => $this->password,
+            'host'     => $this->host,
+            'driver'   => $this->driver,
+        ];
+
+        $this->connection = DriverManager::getConnection($params);
+
+        return $this;
+    }
+
+    /**
+     * @throws \Doctrine\DBAL\Exception
+     */
+    public function createDatabase(string $name): void
+    {
+        $this->connection
+            ->createSchemaManager()
+            ->createDatabase($name);
+    }
+}
