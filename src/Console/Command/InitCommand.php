0a31093fa46dd51ecf224582a9712d051912684b
Jascha030 <contact@jaschavanaalst.nl>
2021-10-03 22:27:06 +0200

    Finish command

--- src/Console/Command/InitCommand.php
+++ src/Console/Command/InitCommand.php
@@ -3,7 +3,9 @@
 namespace Jascha030\Xerox\Console\Command;
 
 use Exception;
+use Jascha030\Xerox\Composer\ComposerHelper;
 use Jascha030\Xerox\Database\DatabaseService;
+use Jascha030\Xerox\Twig\TwigTemplater;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -11,9 +13,17 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Process\Process;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 final class InitCommand extends Command
 {
+    private const SALTS_URL   = "https://api.wordpress.org/secret-key/1.1/salt";
+    private const CONST_REGEX = "/define\('([A-Z_]*)',[ \t]*'(.*)'\);/";
+
     private ContainerInterface $container;
 
     public function __construct(ContainerInterface $container) {
@@ -36,6 +46,8 @@ final class InitCommand extends Command
         $user     = $this->ask($input, $output, 'user');
         $password = $this->ask($input, $output, 'password');
 
+        $output->writeln('Creating database...');
+
         try {
             (new DatabaseService($user, $password))->createDatabase($database);
         } catch (Exception $e) {
@@ -44,7 +56,83 @@ final class InitCommand extends Command
             return Command::FAILURE;
         }
 
+        $url = $this->ask($input, $output, 'url');
+
+        try {
+            $this->generateDotEnv($database, $user, $password, 'https://' . $url . '.test');
+        } catch (LoaderError | RuntimeError | SyntaxError $e) {
+            $output->writeln($e->getMessage());
+
+            return Command::FAILURE;
+        }
+
+        $this->valetLink($url, $output);
+
         return Command::SUCCESS;
+    }
+
+    private function valetLink(string $domain, OutputInterface $output): void
+    {
+        $process = Process::fromShellCommandline("valet link {$domain}");
+
+        $process->run(static function ($type, $buffer) use ($output) {
+            $output->writeln($buffer);
+        });
+    }
+
+    /**
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     * @throws \Twig\Error\LoaderError
+     */
+    private function generateDotEnv(string $database, string $user, string $password, string $url): void
+    {
+        /** @var TwigTemplater $templater */
+        $templater = $this->container->get(TwigTemplater::class);
+        $salts     = $this->getSalts();
+
+        $envString = $templater->render('env.twig', [
+            'name'     => $database,
+            'user'     => $user,
+            'password' => $password,
+            'url'      => $url,
+            'salts'    => $salts,
+            'debug'    => 'true',
+        ]);
+
+        $root = ComposerHelper::getComposerRoot();
+        $env = $root . '/public/.env';
+
+        if (!file_exists($env)) {
+            (new Filesystem())->touch($env);
+
+            file_put_contents($env, $envString);
+        }
+    }
+
+    private function getSalts(): ?string
+    {
+        $resource = curl_init();
+        curl_setopt($resource, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($resource, CURLOPT_URL, self::SALTS_URL);
+        $data = curl_exec($resource);
+        curl_close($resource);
+
+        preg_match_all(self::CONST_REGEX, $data, $matches);
+
+        if (isset($matches[1], $matches[2])) {
+            $salts = array_combine($matches[1], $matches[2]);
+
+            ob_start();
+
+            foreach ($salts as $key => $value) {
+                echo $key . "='{$value}'" . PHP_EOL;
+            }
+
+            return ob_get_clean();
+        }
+
+        return null;
     }
 
     private function getQuestion(string $question): Question
