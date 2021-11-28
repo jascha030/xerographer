d71345dc01b04acf300a6d33b194f2a1f188e455
Jascha030 <contact@jaschavanaalst.nl>
2021-11-28 21:58:31 +0100

    Add Configure and salts tests

--- tests/Console/Command/InitCommandTest.php
+++ tests/Console/Command/InitCommandTest.php
@@ -7,22 +7,45 @@ use Jascha030\Xerox\Console\Command\InitCommand;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Command\Command;
 
-class InitCommandTest extends TestCase
+final class InitCommandTest extends TestCase
 {
+    private const WP_CONFIG_CONSTANTS = [
+        'AUTH_KEY',
+        'SECURE_AUTH_KEY',
+        'LOGGED_IN_KEY',
+        'NONCE_KEY',
+        'AUTH_SALT',
+        'SECURE_AUTH_SALT',
+        'LOGGED_IN_SALT',
+        'NONCE_SALT',
+    ];
+
     /**
      * @throws Exception
      */
     public function testConstruct(): InitCommand
     {
-        require_once dirname(__FILE__, 4) . '/includes/bootstrap.php';
-
-        $container = getContainer();
+        $container = include dirname(__FILE__, 4) . '/includes/bootstrap.php';
         $command   = new InitCommand($container);
 
         self::assertInstanceOf(Command::class, $command);
         self::assertInstanceOf(InitCommand::class, $command);
 
         return $command;
+    }
+
+    /**
+     * @depends testConstruct
+     */
+    public function testConfigure(InitCommand $command): void
+    {
+        $description = 'Init a new Environment with database.';
+
+        $command->setDescription('test');
+        self::assertEquals('test', $command->getDescription());
+
+        $command->configure();
+        self::assertEquals($description, $command->getDescription());
     }
 
     /**
@@ -41,8 +64,19 @@ class InitCommandTest extends TestCase
         $salts = $init->getSalts();
 
         self::assertIsString($salts);
-        // print_r($salts);
-        // die();
-    }
 
+        $lines = explode(PHP_EOL, $salts);
+        self::assertCount(9, $lines);
+        self::assertEquals('', $lines[8]);
+
+        array_pop($lines);
+
+        $constants = [];
+
+        foreach ($lines as $line) {
+            $constants[] = substr($line, 0, strpos($line, '='));
+        }
+
+        self::assertEquals(self::WP_CONFIG_CONSTANTS, $constants);
+    }
 }
