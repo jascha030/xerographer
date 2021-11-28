83121006d275faffc5d0007f54675cec220d5456
Jascha030 <contact@jaschavanaalst.nl>
2021-11-29 00:14:24 +0100

    Test enc to compare against

---
+++ tests/Application/ApplicationTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Jascha030\Xerox\Tests\Application;
+
+use Jascha030\Xerox\Application\Application;
+use PHPUnit\Framework\TestCase;
+
+class ApplicationTest extends TestCase
+{
+
+}
