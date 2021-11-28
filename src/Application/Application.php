b67f8530d9d4b08ba5e3ee1622fbee6e1e1361ad
Jascha030 <contact@jaschavanaalst.nl>
2021-11-28 21:00:30 +0100

    Create library specific ConsoleApplication class

--- src/Application/Application.php
+++ src/Application/Application.php
@@ -2,7 +2,25 @@
 
 namespace Jascha030\Xerox\Application;
 
-class XerographerApplication
-{
+use Jascha030\Xerox\Xerographer;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Console\Application as ConsoleApplication;
 
+final class Application extends ConsoleApplication
+{
+    private ContainerInterface $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+
+        parent::__construct(Xerographer::APP_NAME, Xerographer::APP_VERSION);
+    }
+
+    public function init(): void
+    {
+        foreach (Xerographer::APP_COMMANDS as $class) {
+            $this->add(new $class($this->container));
+        }
+    }
 }
