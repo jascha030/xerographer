bc7e64daa4cea1b370c85085a04160afad1c2160
Jascha030 <contact@jaschavanaalst.nl>
2021-11-28 20:15:34 +0100

    Create Templater Tests

--- tests/Twig/TwigTemplaterTest.php
+++ tests/Twig/TwigTemplaterTest.php
@@ -2,10 +2,45 @@
 
 namespace Jascha030\Xerox\Tests\Twig;
 
+use Jascha030\Xerox\Twig\TemplaterInterface;
 use Jascha030\Xerox\Twig\TwigTemplater;
 use PHPUnit\Framework\TestCase;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 class TwigTemplaterTest extends TestCase
 {
+    public function testConstruct(): TemplaterInterface
+    {
+        $fileSystem  = new FilesystemLoader(dirname(__DIR__) . '/Fixtures/Twig');
+        $environment = new Environment($fileSystem);
 
+        $templater = new TwigTemplater($environment);
+
+        self::assertInstanceOf(TemplaterInterface::class, $templater);
+
+        return $templater;
+    }
+
+    /**
+     * @depends testConstruct
+     */
+    public function testRender(TemplaterInterface $templater): void
+    {
+        $renderedOutput = $templater->render('test-template.twig', [
+            'location' => 'world'
+        ]);
+
+        self::assertIsString($renderedOutput);
+        self::assertEquals('<p>Hello world!</p>' . PHP_EOL, $renderedOutput);
+    }
+
+    /**
+     * @depends testConstruct
+     */
+    public function testGetEnvironment(TemplaterInterface $templater): void
+    {
+        /** @noinspection UnnecessaryAssertionInspection */
+        self::assertInstanceOf(Environment::class, $templater->getEnvironment());
+    }
 }
