7b35aa669eae8fb2836b0b2c42e358b4a3932120
Jascha030 <contact@jaschavanaalst.nl>
2021-11-28 21:00:51 +0100

    Create simple class providing App information.

--- src/Xerographer.php
+++ src/Xerographer.php
@@ -2,7 +2,39 @@
 
 namespace Jascha030\Xerox;
 
-class Xerographer
-{
+use Jascha030\Xerox\Console\Command\InitCommand;
 
+final class Xerographer
+{
+    /**
+     * Console app version.
+     */
+    public const APP_VERSION = '1.0.0';
+
+    /**
+     * Console app name.
+     */
+    public const APP_NAME = 'Xerographer';
+
+    /**
+     * Console app commands
+     */
+    public const APP_COMMANDS = [
+        InitCommand::class
+    ];
+
+    /**
+     * Get definition files for the Container injected in the Console Application.
+     *
+     * @return string[]
+     */
+    public static function getConfigurationFiles(): array
+    {
+        $configDir = dirname(__DIR__) . '/config';
+
+        return [
+            "{$configDir}/console.php",
+            "{$configDir}/twig.php"
+        ];
+    }
 }
