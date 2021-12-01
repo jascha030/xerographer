dc669fa285f0d92444000899250aa642c01ea9b4
Jascha030 <contact@jaschavanaalst.nl>
2021-12-01 20:00:10 +0100

    Add new bootstrap.php for phpunit to make sure the Init command test is ran inside the fake project dir.

--- tests/Fixtures/testproject/bootstrap.php
+++ tests/Fixtures/testproject/bootstrap.php
@@ -1,1 +1,20 @@
 <?php
+
+/**
+ * PHPUnit bootstrap file.
+ */
+declare(strict_types=1);
+
+/**
+ * Load the container and composer autoloader.
+ */
+$container = include dirname(__FILE__, 4) . '/includes/bootstrap.php';
+
+/**
+ * Change working directory to current dir.
+ * This is used during execution of `InitCommandTest::testExecute()`,
+ * which uses the Symfony CommandTester to execute the init command in this directory.
+ * When the test is executed successfully a .env file will temporarily be created in ./public.
+ * This file will thereafter be removed by the `InitCommandTest::tearDown()` method.
+ */
+chdir(__DIR__);
