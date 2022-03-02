8ec35783800177227d907e21fd4afbcfe3354be8
Jascha030 <contact@jaschavanaalst.nl>
2022-03-02 22:38:05 +0100

    🔧 Consistency in definition files.

--- config/twig.php
+++ config/twig.php
@@ -11,11 +11,8 @@ use function DI\create;
 use function DI\get;
 
 return [
-    'twig.root'            => dirname(__DIR__) . '/templates',
-    LoaderInterface::class => create(FilesystemLoader::class)
-        ->constructor(get('twig.root')),
-    Environment::class => create(Environment::class)
-        ->constructor(get(LoaderInterface::class)),
-    TemplaterInterface::class => create(TwigService::class)
-        ->constructor(get(Environment::class)),
+    'twig.root'               => dirname(__DIR__) . '/templates',
+    LoaderInterface::class    => create(FilesystemLoader::class)->constructor(get('twig.root')),
+    Environment::class        => create(Environment::class)->constructor(get(LoaderInterface::class)),
+    TemplaterInterface::class => create(TwigService::class)->constructor(get(Environment::class)),
 ];
