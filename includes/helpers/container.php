6d443f104f20b27dedbaa19d31f77658e5f14acc
Jascha030 <contact@jaschavanaalst.nl>
2022-03-03 00:08:23 +0100

    ✨ Create container helper functions

--- includes/helpers/container.php
+++ includes/helpers/container.php
@@ -1,1 +1,54 @@
 <?php
+
+declare(strict_types=1);
+
+namespace Jascha030\Xerox\Helpers;
+
+use DI\ContainerBuilder;
+use Psr\Container\ContainerInterface;
+
+/**
+ * Scan the ./definitions directory for definition files.
+ *
+ * @noinspection PhpUnnecessaryLocalVariableInspection
+ */
+function definitions(?string $dir = null): \Generator
+{
+    $dir ?? $dir = dirname(__FILE__, 3) . '/config';
+    $files       = array_diff(scandir($dir), ['..', '.']);
+
+    foreach ($files as $file) {
+        if (! str_ends_with($file, '.php')) {
+            continue;
+        }
+
+        $basename = str_replace('.php', '', $file);
+
+        yield $basename => "{$dir}/{$file}";
+    }
+}
+
+/**
+ * Bootstrap a new Di Container.
+ *
+ * @noinspection PhpUnhandledExceptionInspection
+ */
+function createContainer(): ContainerInterface
+{
+    $builder = (new ContainerBuilder())
+        ->useAutowiring(false)
+        ->useAnnotations(false);
+
+    foreach (definitions() as $definition) {
+        $builder->addDefinitions($definition);
+    }
+
+    return $builder->build();
+}
+
+function container(): ContainerInterface
+{
+    static $container;
+
+    return $container ?? $container = createContainer();
+}
