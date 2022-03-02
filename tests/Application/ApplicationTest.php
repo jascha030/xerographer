5142836e995474ec2d403988771f23c65ab72d7a
Jascha030 <contact@jaschavanaalst.nl>
2022-03-03 00:14:38 +0100

    ✅ Update ApplicationTest to work with refactor.

--- tests/Application/ApplicationTest.php
+++ tests/Application/ApplicationTest.php
@@ -4,26 +4,29 @@ declare(strict_types=1);
 
 namespace Jascha030\Xerox\Tests\Application;
 
-use Jascha030\Xerox\Application\Application;
 use Jascha030\Xerox\Console\Command\InitCommand;
 use PHPUnit\Framework\TestCase;
-use Psr\Container\ContainerInterface;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Exception\CommandNotFoundException;
 use Symfony\Component\Console\Tester\ApplicationTester;
 use Symfony\Contracts\Service\ResetInterface;
+use function Jascha030\Xerox\Helpers\container;
 
 /**
  * @covers \Jascha030\Xerox\Xerographer
+ *
  * @internal
  */
 final class ApplicationTest extends TestCase
 {
+    /**
+     * @throws ContainerExceptionInterface|NotFoundExceptionInterface
+     */
     public function testConstruct(): ResetInterface
     {
-        $container = include dirname(__FILE__, 3) . '/includes/bootstrap.php';
-        $this->assertInstanceOf(ContainerInterface::class, $container);
-
-        $app = new Application($container);
+        $app = container()->get('app');
 
         $this->assertInstanceOf(ResetInterface::class, $app);
         $this->assertInstanceOf(Application::class, $app);
@@ -34,19 +37,8 @@ final class ApplicationTest extends TestCase
     /**
      * @depends testConstruct
      */
-    public function testGetThrowsException(Application $app): void
-    {
-        $this->expectException(CommandNotFoundException::class);
-        $app->get('init');
-    }
-
-    /**
-     * @depends testConstruct
-     * @depends testGetThrowsException
-     */
     public function testAddXerographerCommands(Application $app): void
     {
-        $app->addXerographerCommands();
         $initCommand = $app->get('init');
 
         $this->assertInstanceOf(InitCommand::class, $initCommand);
