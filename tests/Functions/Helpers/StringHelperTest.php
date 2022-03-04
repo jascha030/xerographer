d17874428acfdd9b312700599573c7a2f3c18a36
Jascha030 <contact@jaschavanaalst.nl>
2022-03-04 11:27:40 +0100

    ✅ Add unittest for new string helper

--- tests/Functions/Helpers/StringHelperTest.php
+++ tests/Functions/Helpers/StringHelperTest.php
@@ -1,8 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jascha030\Xerox\Tests\Functions\Helpers;
 
-class Strj
-{
+use PHPUnit\Framework\TestCase;
+use function Jascha030\Xerox\Helpers\sanitizeProjectName;
 
+/**
+ * @internal
+ * @coversNothing
+ */
+final class StringHelperTest extends TestCase
+{
+    public function testSanitizeProjectName(): void
+    {
+        $expected = 'nice-php8-project-name';
+
+        $this->assertEquals($expected, sanitizeProjectName('NICE PHP8 \\\'Project NAME\\\''));
+    }
 }
