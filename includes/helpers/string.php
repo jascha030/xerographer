ef6511f08bf4d66bebf335500c9a497c61def3f1
Jascha030 <contact@jaschavanaalst.nl>
2022-03-04 11:26:14 +0100

    ✨ Add string helpers file with projectname sanitize function.

--- includes/helpers/string.php
+++ includes/helpers/string.php
@@ -1,8 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jascha030\Xerox\Helpers;
 
-class string
+function sanitizeProjectName(string $string): string
 {
+    /** @noinspection RegExpRedundantEscape */
+    $regex = '([^\\w\\s\\d\\-_~,;\\[\\]\\(\\).])';
 
+    return strtolower(str_replace(' ', '-', preg_replace($regex, '', $string)));
 }
