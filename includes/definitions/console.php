45d0a7608ae2a3083d8cd5e84631764dc6e72599
Jascha030 <contact@jaschavanaalst.nl>
2022-03-04 11:28:08 +0100

    ✨ Add starter command.

--- includes/definitions/console.php
+++ includes/definitions/console.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Jascha030\Twig\Templater\TemplaterInterface;
 use Jascha030\Xerox\Console\Command\InitCommand;
+use Jascha030\Xerox\Console\Command\StarterCommand;
 use Jascha030\Xerox\Xerographer;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Question\Question;
@@ -19,5 +20,8 @@ return [
     'command.init.questions.user'     => create(Question::class)->constructor('Enter mysql username: '),
     'command.init.questions.password' => create(Question::class)->constructor('Password: ')->method('setHidden', true)->method('setHiddenFallback', false),
     'command.init.questions.url'      => create(Question::class)->constructor('Enter url domain name: '),
-    'commands'                        => ['init' => create(InitCommand::class)->constructor(get(TemplaterInterface::class))],
+    'commands'                        => [
+        'init'    => create(InitCommand::class)->constructor(get(TemplaterInterface::class)),
+        'starter' => create(StarterCommand::class),
+    ],
 ];
--- src/Console/Command/StarterCommand.php
+++ src/Console/Command/StarterCommand.php
@@ -1,8 +1,43 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jascha030\Xerox\Console\Command;
 
-class StarterCommand
-{
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+use function Jascha030\Xerox\Helpers\sanitizeProjectName;
 
+final class StarterCommand extends Command
+{
+    public function __construct()
+    {
+        parent::__construct('starter');
+    }
+
+    public function configure(): void
+    {
+        $this->addArgument('name', InputArgument::REQUIRED, 'Project name.');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output): ?int
+    {
+        $project = sanitizeProjectName($input->getArgument('name'));
+
+        $process = Process::fromShellCommandline("`which composer` create-project jascha030/wp-starter {$project}", getcwd());
+        $process->start();
+
+        foreach ($process as $type => $line) {
+            if (Process::ERR === $type) {
+                $line = sprintf('<error>%s</error>', $line);
+            }
+
+            $output->writeln($line);
+        }
+
+        return $process->getExitCode();
+    }
 }
